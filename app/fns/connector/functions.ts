db34517ef0626444dfcb41acd74d28aaa9ecd496
soorajshankar <soorajshankar@gmail.com>
2024-03-29 11:42:03 +0530

    added Type2Command Relationship

--- app/fns/connector/functions.ts
+++ app/fns/connector/functions.ts
@@ -4,3 +4,28 @@
 export function hello(name?: string) {
   return `hello ${name ?? "world"}`;
 }
+
+
+interface Price {
+  /** Price in given currency */
+  amount: number;
+  /** currency */
+  currency: string;
+}
+
+/**
+ * fetches the price in given currency
+ *
+ * @param amount amount friom
+ * @param to_currency to which currency to convert
+ * @readonly
+ */
+export async function getPriceInLocalCurrency(
+  amount: number,
+  to_currency: string = "USD"
+): Promise<Price> {
+  return {
+    amount: amount * 1.1, // replace with actual conversion logic
+    currency: to_currency,
+  };
+}
